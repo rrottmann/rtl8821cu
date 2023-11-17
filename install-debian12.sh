646bd09e891c7b93cbfe8b1df644ad2e4dd99023
Reiner Rottmann <reiner@rottmann.it>
2023-11-17 21:03:12 +0100

    Create install-debian12.sh
    Added script for installation on Debian 12.

---
+++ install-debian12.sh
@@ -0,0 +1,13 @@
+sudo apt update
+which bc || sudo apt install -y bc
+which dkms || sudo apt install -y dkms
+which git || sudo apt install -y git
+sudo apt install -y linux-headers-$(uname -r)
+mkdir ~/git
+cd ~/git
+#git clone https://github.com/Rhizomatica/rtl8821cu.git
+git clone https://github.com/rrottmann/rtl8821cu.git
+cd rtl8821cu
+make
+sudo make install
+sudo modprobe 8821cu.ko
